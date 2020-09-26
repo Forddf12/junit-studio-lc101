1253b8aecebcdbbaf9ac38babfddc51aea08a070
Doug F <forddf12@outlook.com>
2020-09-26 16:21:44 -0500

    Final

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -9,19 +9,59 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
-    }
-
-    @Test
     public void onlyBracketsReturnsTrue() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
     @Test
-    public void
+    public void bracketsWithTextInMiddle() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
 
+    @Test
+    public void bracketsStartInTheMiddleOfText() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
 
+    @Test
+    public void bracketFollowedByText() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void openBracketFollowedByTextNoClose() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void textCloseBracketTextOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void openBracketNoClose() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void closeBracketOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void nestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void deepNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[ []]]]"));
+    }
 }
 
 
